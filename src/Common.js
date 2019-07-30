8c870ec4c5075c55450247b02c22caccbe3a0883
yanxiaojun617 <yanxiaojun617@163.com>
2019-07-30 22:03:59 +0800

    style

---
+++ src/Common.js
@@ -0,0 +1,5 @@
+import 'jquery';
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+import 'font-awesome/scss/font-awesome.scss';
+
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './Common'
+import './assets/setting/easyui'
+
+$('#index').html('iii_index.js');
+console.log('iii_index.js');
+alert('iii_index.js');
